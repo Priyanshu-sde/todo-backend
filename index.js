692a718fada7f81923fc633785da60864c06b990
Priyanshu.sde <priyanshu.sde@gmail.com>
2024-11-08 23:31:34 +0530

    sdf

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import express from 'express';
-import connectDB from './db/db.js';
-import userRoutes from './routes/users.js';
-import TodoRoutes from './routes/todos.js';
-import cors from 'cors';
+import express from "express";
+import connectDB from "./db/db.js";
+import userRoutes from "./routes/users.js";
+import TodoRoutes from "./routes/todos.js";
+import cors from "cors";
 
 const app = express();
-app.use(cors({ origin: 'https://grand-tapioca-57e48b.netlify.app'}));
+app.use(cors({ origin: "https://grand-tapioca-57e48b.netlify.app" }));
+app.use(
+  cors({
+    origin: "https://grand-tapioca-57e48b.netlify.app",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 connectDB();
 
 app.use(express.json());
@@ -13,5 +21,5 @@ app.use(userRoutes);
 app.use(TodoRoutes);
 
 app.listen(3000, () => {
-    console.log('server on ');
-})
+  console.log("server on ");
+});
